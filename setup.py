a43361b391fc7cb65de7ca4102464339f2c6d76a
David Mallou <davidmallouacebal@gmail.com>
2025-06-13 19:27:55 +0200

    Subida de los archivos de la libreria

---
+++ setup.py
@@ -0,0 +1,22 @@
+# Este archivo setup.py se utiliza para empaquetar la librería
+
+from setuptools import setup, find_packages
+setup(
+    name="barajas",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    author="David Mallou",
+    author_email="davidmallouacebal@gmail.com",
+    description="Una librería de barajas de cartas para Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mallou90/proyecto_barajas",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+) 
---
+++ src/baraja/__init__.py
@@ -0,0 +1,9 @@
+"""
+Paquete de barajas de cartas.
+"""
+from .barajas import Baraja
+from .baraja_española import BarajaEspañola
+from .baraja_francesa import BarajaFrancesa
+
+__version__ = "0.1.0"
+__all__ = ['Baraja', 'BarajaEspañola', 'BarajaFrancesa']
